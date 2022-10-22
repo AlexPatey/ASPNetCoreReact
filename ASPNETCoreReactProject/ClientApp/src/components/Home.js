fc081ea0b823bab20768c98a13e2b96b0cfa4e70
= <alex.patey@btinternet.com>
2022-10-22 16:24:18 +0100

    restyled site and removed navbar

--- ASPNETCoreReactProject/ClientApp/src/components/Home.js
+++ ASPNETCoreReactProject/ClientApp/src/components/Home.js
@@ -68,7 +68,6 @@ class Board extends React.Component {
 
         return (
             <div>
-                {/*<div className="status">{status}</div>*/}
                 <div className="board-row">
                     {this.renderSquare(0)}
                     {this.renderSquare(1)}
@@ -138,7 +137,7 @@ class Game extends React.Component {
                 "Go to game start";
             return (
                 <li key={move }>
-                    <button onClick={() => this.jumpTo(move)}>{desc}</button>
+                    <button className=" btn btn-primary historyBtn" onClick={() => this.jumpTo(move)}>{desc}</button>
                 </li>
             );
         });
@@ -152,15 +151,15 @@ class Game extends React.Component {
         }
 
         return (
-            <div className="game">
-                <div className="game-board">
+            <div className="game row">
+                <div className="game-board col-md-6 col-sm-12">
                     <Board
                         squares={current.squares}
                         onClick={(i) => this.handleClick(i) }
                     />
                 </div>
-                <div className="game-info">
-                    <div>{status}</div>
+                <div className="game-info col-md-6 col-sm-12">
+                    <div className="statusName">{status}</div>
                     <ol>{moves}</ol>
                 </div>
             </div>
@@ -173,23 +172,10 @@ export class Home extends Component {
 
   render() {
     return (
-      <div>
-        {/*<h1>Hello, world!</h1>*/}
-        {/*<p>Welcome to your new single-page application, built with:</p>*/}
-        {/*<ul>*/}
-        {/*  <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>*/}
-        {/*  <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>*/}
-        {/*  <li><a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling</li>*/}
-        {/*</ul>*/}
-        {/*<p>To help you get started, we have also set up:</p>*/}
-        {/*<ul>*/}
-        {/*  <li><strong>Client-side navigation</strong>. For example, click <em>Counter</em> then <em>Back</em> to return here.</li>*/}
-        {/*  <li><strong>Development server integration</strong>. In development mode, the development server from <code>create-react-app</code> runs in the background automatically, so your client-side resources are dynamically built on demand and the page refreshes when you modify any file.</li>*/}
-        {/*  <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and your <code>dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files.</li>*/}
-        {/*</ul>*/}
-        {/*<p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. If you open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>*/}
-        <Game />
-      </div>
+        <div>
+            <div className="projTitle">Alex Patey's Tic Tac Toe</div>
+            <Game />
+        </div>
     );
   }
 }
